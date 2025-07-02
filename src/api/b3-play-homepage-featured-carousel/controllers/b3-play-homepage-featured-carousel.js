d8c31419546aa4ea53091ac57c759851512f7c60
Mitch Myburgh <mitchmyburgh@gmail.com>
2025-07-02 13:21:14 +0200

    Add new B3 Play Homepage Featured Carousel API with controller, service, and routes

---
+++ src/api/b3-play-homepage-featured-carousel/controllers/b3-play-homepage-featured-carousel.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * homepage-featured-carousel controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController(
+  "api::b3-play-homepage-featured-carousel.b3-play-homepage-featured-carousel"
+);
---
+++ src/api/b3-play-homepage-featured-carousel/services/b3-play-homepage-featured-carousel.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * homepage-featured-carousel service
+ */
+
+const { createCoreService } = require("@strapi/strapi").factories;
+
+module.exports = createCoreService(
+  "api::b3-play-homepage-featured-carousel.b3-play-homepage-featured-carousel"
+);
